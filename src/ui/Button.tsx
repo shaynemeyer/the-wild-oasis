850bcbb5ddc1b2d7d68e42e2f8252b08cd41b249
Shayne Meyer <shayne.meyer@gmail.com>
2023-07-07 18:55:04 -0700

    upload nate look and feel

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 
-const sizes = {
+export const sizes = {
   small: css`
     font-size: 1.2rem;
     padding: 0.4rem 0.8rem;
@@ -20,7 +20,7 @@ const sizes = {
   `,
 };
 
-const variations = {
+export const variations = {
   primary: css`
     color: var(--color-brand-50);
     background-color: var(--color-brand-600);
@@ -47,3 +47,19 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
+`;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
+export default Button;
